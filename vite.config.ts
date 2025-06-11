bec8f7c07962789015ec6ddc665143895553b68a
GlebUrv <punkhoygleb@mail.ru>
2025-06-11 19:54:21 +0500

    fixed a few errors + updated readme

--- vite.config.ts
+++ vite.config.ts
@@ -1,59 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import { VitePWA } from "vite-plugin-pwa";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    VitePWA({
-      registerType: "autoUpdate",
-      injectRegister: "auto",
-      devOptions: {
-        enabled: true,
-      },
-      workbox: {
-        globPatterns: ["*/.{js,ts,tsx,css,html,ico,png,svg,jpg,jpeg}"],
-        cleanupOutdatedCaches: true,
-        maximumFileSizeToCacheInBytes: 3000000,
-      },
-      manifest: {
-        name: "MentorExchange",
-        short_name: "MEx",
-        icons: [
-          {
-            src: "/MEx_192x192.png",
-            sizes: "192x192",
-            type: "image/png",
-            purpose: "any",
-          },
-          {
-            src: "/MEx_512x512.png",
-            sizes: "512x512",
-            type: "image/png",
-            purpose: "any",
-          },
-          {
-            src: "/MEx_192x192.png",
-            sizes: "192x192",
-            type: "image/png",
-            purpose: "maskable",
-          },
-          {
-            src: "/MEx_512x512.png",
-            sizes: "512x512",
-            type: "image/png",
-            purpose: "maskable",
-          },
-        ],
-        start_url: "/",
-        display: "standalone",
-        background_color: "#ffffff",
-        theme_color: "#ffffff",
-      },
-    }),
-  ],
+  plugins: [react()],
   resolve: {
     alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }],
   },
